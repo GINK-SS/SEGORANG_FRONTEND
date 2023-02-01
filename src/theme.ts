8bcee194bd627d8cc788e6c4b77ace3d651f2213
Sangmin Lee <gink.ss00@gmail.com>
2023-02-01 15:02:40 +0900

    feat: 색상 설정 및 reset CSS

---
+++ src/theme.ts
@@ -0,0 +1,8 @@
+import { DefaultTheme } from 'styled-components';
+
+export const theme: DefaultTheme = {
+  textColor: '#000',
+  sejongCrimsonRed: 'rgba(195, 0, 47)',
+  sejongGray: 'rgba(81, 98, 111)',
+  accentColor: '#990011',
+};
