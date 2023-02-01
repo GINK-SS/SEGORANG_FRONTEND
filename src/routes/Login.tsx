bc776c5df1fa3d554bf552491e4e590416b091a9
Sangmin Lee <gink.ss00@gmail.com>
2023-02-01 16:46:33 +0900

    feat: 로그인 폼 구현
    react-hook-form 사용
    아이디 비밀번호 둘 다 입력하지 않으면 로그인 버튼 비활성화
    로그인 버튼 클릭 시 폼에 작성한 아이디 비밀번호 입력 값 삭제 (추후, 로그인 정상 완료 시 삭제로 수정 예정)

--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -1,5 +1,47 @@
+import { useForm } from 'react-hook-form';
+import styled from 'styled-components';
+
+const LoginForm = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
+const LoginInput = styled.input``;
+
+interface ILoginForm {
+  userId: string;
+  userPw: string;
+}
+
 function Login() {
-  return <div>sample</div>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    watch,
+    reset,
+  } = useForm<ILoginForm>();
+
+  const submitLoginInput = () => {
+    console.log('서버 전송 후 일치 여부 확인! 없으면 에러 메세지 표시!');
+    reset({ userId: '', userPw: '' });
+  };
+
+  return (
+    <LoginForm onSubmit={handleSubmit(submitLoginInput)}>
+      <LoginInput
+        {...register('userId', { required: '아이디를 입력하세요' })}
+        type="text"
+        placeholder="아이디"
+      />
+      <LoginInput
+        {...register('userPw', { required: '비밀번호를 입력하세요' })}
+        type="password"
+        placeholder="비밀번호"
+      />
+      <span>{errors?.userId?.message || errors?.userPw?.message}</span>
+      <button disabled={!watch('userId') || !watch('userPw')}>로그인</button>
+    </LoginForm>
+  );
 }
 
 export default Login;
