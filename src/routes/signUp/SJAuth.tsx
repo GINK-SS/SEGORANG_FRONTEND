d8cb86c1e420ef3a2021bda6cd5890baab1c1972
Sangmin Lee <gink.ss00@gmail.com>
2023-02-01 18:33:18 +0900

    feat: SJAuth에 학번 비밀번호 입력 폼 구현
    추가 해야할 것)
    세고랑 DB에 존재하는지와 학번 비밀번호가 일치하는지 여부를 파악하는 api 연결해야함

--- src/routes/signUp/SJAuth.tsx
+++ src/routes/signUp/SJAuth.tsx
@@ -1,5 +1,55 @@
+import { useForm } from 'react-hook-form';
+import styled from 'styled-components';
+
+const SignUpForm = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
+const SignUpInput = styled.input``;
+
+interface IInputData {
+  studentId: string;
+  studentPw: string;
+}
+
+interface IInDB {
+  in_db: boolean;
+  msg: string;
+  result: null;
+  status_code: number;
+  status_msg: string;
+}
+
 function SJAuth() {
-  return null;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    watch,
+    reset,
+  } = useForm<IInputData>();
+
+  const submitSJAuthInput = () => {
+    console.log('세고랑 DB 안에 있는지 확인 후 세종대학교 구성원인지 확인!');
+    reset({ studentId: '', studentPw: '' });
+  };
+
+  return (
+    <SignUpForm onSubmit={handleSubmit(submitSJAuthInput)}>
+      <SignUpInput
+        {...register('studentId', { required: '학번을 입력하세요' })}
+        type="text"
+        placeholder="학번"
+      />
+      <SignUpInput
+        {...register('studentPw', { required: '비밀번호를 입력하세요' })}
+        type="password"
+        placeholder="비밀번호"
+      />
+      <span>{errors?.studentId?.message || errors?.studentPw?.message}</span>
+      <button disabled={!watch('studentId') || !watch('studentPw')}>인증하기</button>
+    </SignUpForm>
+  );
 }
 
 export default SJAuth;
