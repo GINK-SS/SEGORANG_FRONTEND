046ee108d80f61fb8dc77b3bc8c37d6cc62a069b
Sangmin Lee <gink.ss00@gmail.com>
2023-03-08 22:48:33 +0900

    feat: 로그인 되어 있으면 로그인 및 회원가입 페이지에서 바로 메인으로 가도록 구현
    토큰을 상태관리하여 이 토큰의 유무에 따라 리다이렉트 할 수 있도록 구현하였다.

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -1,1 +1,18 @@
 import { atom } from 'recoil';
+
+interface IUserInfo {
+  accessToken: string;
+  userName: string;
+  userNickname: string;
+  userMajor: string;
+}
+
+export const userInfoState = atom<IUserInfo>({
+  key: 'userInfo',
+  default: {
+    accessToken: '',
+    userName: '',
+    userNickname: '',
+    userMajor: '',
+  },
+});
--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,21 +1,38 @@
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
+import { useRecoilState } from 'recoil';
+import { userInfoState } from '../atoms';
 import Login from './Login';
 import SignUp from './signUp/SignUp';
 import SJAuth from './signUp/SJAuth';
 
 function Router() {
+  const accessToken = localStorage.getItem('sgrUserToken');
+  const [userInfo, setUserInfo] = useRecoilState(userInfoState);
+
+  if (accessToken) {
+    setUserInfo((prev) => {
+      return {
+        ...prev,
+        accessToken: accessToken,
+      };
+    });
+  }
+
   return (
     <BrowserRouter>
       <Switch>
-        <Route path={'/login'}>
-          <Login />
-        </Route>
-        <Route path={'/signUp'}>
-          <SJAuth />
-        </Route>
-        <Route path={'/signUpForm'}>
-          <SignUp />
-        </Route>
+        <Route
+          path={'/login'}
+          component={() => (userInfo.accessToken ? <Redirect to={'/'} /> : <Login />)}
+        />
+        <Route
+          path={'/signUp'}
+          component={() => (userInfo.accessToken ? <Redirect to={'/'} /> : <SJAuth />)}
+        />
+        <Route
+          path={'/signUpForm'}
+          component={() => (userInfo.accessToken ? <Redirect to={'/'} /> : <SignUp />)}
+        />
       </Switch>
     </BrowserRouter>
   );
