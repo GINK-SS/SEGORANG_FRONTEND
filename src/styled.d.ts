68803e55ca75c9fc7a5d91981fda05272f85c13b
Sangmin Lee <gink.ss00@gmail.com>
2023-03-19 22:23:05 +0900

    feat: 기본 색 추가

--- src/styled.d.ts
+++ src/styled.d.ts
@@ -4,6 +4,7 @@ declare module 'styled-components' {
   export interface DefaultTheme {
     textColor: string;
     sejongCrimsonRed: string;
+    sejongCrimsonRed02: string;
     sejongGray: string;
     accentColor: string;
   }
