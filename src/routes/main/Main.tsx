daa4a0e01f0f468fb5c85d4b29b582ec509d4432
Sangmin Lee <gink.ss00@gmail.com>
2023-03-20 20:13:41 +0900

    feat: 메인 페이지 닉네임, 이름, 전공 표시 구현
    약간의 스타일도 가미.
    물론 수정 필요.

--- src/routes/main/Main.tsx
+++ src/routes/main/Main.tsx
@@ -1,5 +1,49 @@
+import { useRecoilValue } from 'recoil';
+import styled from 'styled-components';
+import { userInfoState } from '../../atoms';
+
+const UserContainer = styled.div``;
+
+const UserInfoWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 350px;
+  align-items: center;
+  justify-content: center;
+  border: 1px solid rgba(0, 0, 0, 0.3);
+  border-radius: 10px;
+  padding: 15px 10px;
+  background-color: rgba(0, 0, 0, 0.03);
+
+  span {
+    &:first-child {
+      color: ${(props) => props.theme.sejongCrimsonRed};
+      font-size: 22px;
+      font-weight: 700;
+      margin-bottom: 15px;
+    }
+
+    &:last-child {
+      color: ${(props) => props.theme.sejongGray};
+      font-size: 14px;
+    }
+  }
+`;
+
 function Main() {
-  return null;
+  const userInfo = useRecoilValue(userInfoState);
+  return (
+    <div>
+      <UserContainer>
+        <UserInfoWrapper>
+          <span>{userInfo.userNickname}</span>
+          <span>
+            {userInfo.userName} / {userInfo.userMajor}
+          </span>
+        </UserInfoWrapper>
+      </UserContainer>
+    </div>
+  );
 }
 
 export default Main;
