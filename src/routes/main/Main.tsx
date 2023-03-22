97fc8c6a265c4bf2f951277b7bcf5c102ec1434a
Sangmin Lee <gink.ss00@gmail.com>
2023-03-23 02:00:30 +0900

    chore: 임시 유튜브 게시물 생성

--- src/routes/main/Main.tsx
+++ src/routes/main/Main.tsx
@@ -548,6 +548,27 @@ function Main() {
     '오늘은 말할거야',
   ];
 
+  const youtubeData = [
+    {
+      thumbNail:
+        'https://i.ytimg.com/vi/__092GlqCUw/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLD4C6IcOdd3cleTDtGQMQCNW8AVnA',
+      title: '[어쩌다 신입생🤔] 20학번의 강제 23학번 체험기❗️',
+      link: 'https://www.youtube.com/watch?v=__092GlqCUw&ab_channel=%EC%84%B8%EC%A2%85%EB%8C%80%ED%95%99%EA%B5%90',
+    },
+    {
+      thumbNail:
+        'https://i.ytimg.com/vi/9Acim_CfG1U/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDnlQAHyLaCbqKofrlUaj4rFN6IRA',
+      title: '[학과 소개 인터뷰📢] EP. 미디어커뮤니케이션학과🎥',
+      link: 'https://www.youtube.com/watch?v=9Acim_CfG1U&ab_channel=%EC%84%B8%EC%A2%85%EB%8C%80%ED%95%99%EA%B5%90',
+    },
+    {
+      thumbNail:
+        'https://i.ytimg.com/vi/lOrNhpOFcQY/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLA9Hpc6FvZM1UkLfaHIw8nYPXji8w',
+      title: '[🛞대학생을 위한 운전면허 꿀팁🛞] 운전면허 독학으로 취득하고 싶다면⁉️',
+      link: 'https://www.youtube.com/watch?v=lOrNhpOFcQY&ab_channel=%EC%84%B8%EC%A2%85%EB%8C%80%ED%95%99%EA%B5%90',
+    },
+  ];
+
   return (
     <>
       <Header>
