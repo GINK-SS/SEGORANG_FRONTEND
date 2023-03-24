4e4ed7eaac469f335f9469e65cd3b334296fe4b4
Sangmin Lee <gink.ss00@gmail.com>
2023-03-25 01:03:15 +0900

    feat: 게시물 상단 스타일 구현
    게시물 상단에는 '게시된 게시판 이름', '제목', '작성자', '작성 날짜', '조회수', '추천수'가 표시된다.

--- src/routes/board/Post.tsx
+++ src/routes/board/Post.tsx
@@ -1,5 +1,65 @@
+import styled from 'styled-components';
 import BoardHeader from '../../components/BoardHeader';
 import NavContainer from '../../components/NavContainer';
+
+const Container = styled.div`
+  max-width: 1300px;
+  margin: 40px auto 20px;
+  border-radius: 10px;
+  padding: 20px 0px;
+  background-color: rgba(0, 0, 0, 0.025);
+`;
+
+const TopWrapper = styled.div`
+  border-bottom: 2px solid rgba(0, 0, 0, 0.1);
+  padding: 0 25px 15px;
+`;
+
+const BoardTitle = styled.p`
+  font-size: 16px;
+  color: ${(props) => props.theme.sejongGray};
+  margin-bottom: 50px;
+`;
+
+const PostTitle = styled.p`
+  text-align: center;
+  font-size: 28px;
+  font-weight: 500;
+  margin-bottom: 30px;
+`;
+
+const PostTopWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  font-size: 18px;
+`;
+
+const PostTopLeft = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const Writer = styled.p`
+  margin-right: 10px;
+  color: #333;
+  font-weight: 500;
+`;
+
+const Date = styled.p`
+  color: ${(props) => props.theme.sejongGray};
+`;
+
+const PostTopRight = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const ViewNum = styled.p``;
+
+const LikeNum = styled.p`
+  margin-left: 10px;
+`;
 
 function Post() {
   return (
