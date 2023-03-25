a398cb5dcb368a0fec7d2bc014d316da92b354de
Sangmin Lee <gink.ss00@gmail.com>
2023-03-25 18:32:58 +0900

    chore: 모킹 추가 설정

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,14 @@ import ReactDOM from 'react-dom/client';
 import { RecoilRoot } from 'recoil';
 import { ThemeProvider } from 'styled-components';
 import App from './App';
+import { worker } from './mocks/browser';
 import { theme } from './theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+worker.start({
+  onUnhandledRequest: 'bypass',
+});
+
 root.render(
   <RecoilRoot>
     <ThemeProvider theme={theme}>
