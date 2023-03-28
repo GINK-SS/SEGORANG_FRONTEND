b147012e288d7b46da2a589237351e61d2666b63
Sangmin Lee <gink.ss00@gmail.com>
2023-03-28 23:39:33 +0900

    feat: 상단 네비 바 선택한 게시판이면 표시 구현

--- src/components/container/board/NavContainer.tsx
+++ src/components/container/board/NavContainer.tsx
@@ -1,60 +1,63 @@
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import styled from 'styled-components';
+import { BoardURLParams } from '../../../types/board';
 
 function NavContainer() {
+  const { boardTitle }: BoardURLParams = useParams();
+
   return (
     <ContainerOuter>
       <Container>
         <Wrapper>
-          <Item>
+          <Item isCurrentBoard={boardTitle === 'hot'}>
             <Link to="/board/hot">HOT 게시판</Link>
           </Item>
-          <Item>
+          <Item isCurrentBoard={boardTitle === 'bulletin'}>
             <Link to="/board/bulletin">자유 게시판</Link>
           </Item>
-          <Item>
+          <Item isCurrentBoard={boardTitle === 'anonymous'}>
             <Link to={'/board/anonymous'}>익명 게시판</Link>
           </Item>
-          <Item>
+          <Item isCurrentBoard={boardTitle === 'student'}>
             <Link to={'/board/student'}>재학생 게시판</Link>
           </Item>
-          <Item>
+          <Item isCurrentBoard={boardTitle === 'graduate'}>
             <Link to={'/board/graduate'}>졸업생 게시판</Link>
           </Item>
         </Wrapper>
         <Wrapper>
-          <Item>
+          <Item isCurrentBoard={boardTitle === 'lecture-evaluation'}>
             <Link to={'/board/lecture-evaluation'}>강의 평가</Link>
           </Item>
-          <Item>
+          <Item isCurrentBoard={boardTitle === 'sejong-news'}>
             <Link to={'/board/sejong-news'}>세종 뉴스</Link>
           </Item>
-          <Item>
+          <Item isCurrentBoard={boardTitle === 'sejong-council'}>
             <Link to={'/board/sejong-council'}>세종 학생회</Link>
           </Item>
         </Wrapper>
         <Wrapper>
-          <Item>
+          <Item isCurrentBoard={boardTitle === 'club'}>
             <Link to={'/board/club'}>동아리</Link>
           </Item>
-          <Item>
+          <Item isCurrentBoard={boardTitle === 'study'}>
             <Link to={'/board/study'}>스터디</Link>
           </Item>
-          <Item>
+          <Item isCurrentBoard={boardTitle === 'market'}>
             <Link to={'/board/market'}>장터</Link>
           </Item>
-          <Item>
+          <Item isCurrentBoard={boardTitle === 'job'}>
             <Link to={'/board/job'}>알바 · 과외</Link>
           </Item>
-          <Item>
+          <Item isCurrentBoard={boardTitle === 'travel'}>
             <Link to={'/board/travel'}>여행</Link>
           </Item>
         </Wrapper>
         <Wrapper>
-          <Item>
+          <Item isCurrentBoard={boardTitle === 'notice'}>
             <Link to={'/board/notice'}>공지사항</Link>
           </Item>
-          <Item>
+          <Item isCurrentBoard={boardTitle === 'inquiry'}>
             <Link to={'/board/inquiry'}>운영자 문의</Link>
           </Item>
         </Wrapper>
@@ -93,13 +96,14 @@ const Wrapper = styled.ul`
   }
 `;
 
-const Item = styled.li`
+const Item = styled.li<{ isCurrentBoard: boolean }>`
   margin-bottom: 20px;
   font-weight: 500;
   cursor: pointer;
 
   a {
-    color: rgba(0, 0, 0, 0.8);
+    color: ${({ isCurrentBoard, theme }) =>
+      isCurrentBoard ? theme.accentColor : 'rgba(0, 0, 0, 0.8)'};
 
     &:hover {
       color: ${(props) => props.theme.accentColor};
