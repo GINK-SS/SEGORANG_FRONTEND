d34360c11ec8ae55dc9c363e16ea444c58e7a95f
Sangmin Lee <gink.ss00@gmail.com>
2023-05-20 22:05:59 +0900

    refactor: [메인] 유저란 컴포넌트 분리

--- src/container/main/Aside.tsx
+++ src/container/main/Aside.tsx
@@ -1,5 +1,30 @@
+import { useHistory } from 'react-router-dom';
+import { useRecoilValue, useResetRecoilState } from 'recoil';
+import { userInfoState } from '../../atoms';
+import User from '../../components/main/aside/User';
+
 const Aside = () => {
-  return <div>aside</div>;
+  const history = useHistory();
+  const { userNickname, userName, userMajor } = useRecoilValue(userInfoState);
+  const resetUserInfo = useResetRecoilState(userInfoState);
+
+  const onBookMark = () => console.log('북마크 클릭');
+  const onMyPage = () => console.log('마이페이지 클릭');
+  const onLogout = () => {
+    localStorage.removeItem('sgrUserToken');
+    resetUserInfo();
+    history.replace('/login');
+  };
+  return (
+    <User
+      nickname={userNickname}
+      name={userName}
+      major={userMajor}
+      onBookMark={onBookMark}
+      onMyPage={onMyPage}
+      onLogout={onLogout}
+    />
+  );
 };
 
 export default Aside;
